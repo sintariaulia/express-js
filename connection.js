14feab50f21a40ccca357452dbdfc0a87d130dd9
sintariaulia <sintariaulia04@gmail.com>
2023-05-10 16:29:21 +0700

    feat: update file connection.js

--- connection.js
+++ connection.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql');
 const env = require('dotenv').config();
-const {DB_HOST, DB_USER, DB_PASSWORD, DB_NAME} = process.env;
 
 const connection = mysql.createConnection({
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_NAME
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 connection.connect((err) => {
