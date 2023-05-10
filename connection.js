a80ac5fa533cf48e9df96fc6c0cb77792fe558f0
sintariaulia <sintariaulia04@gmail.com>
2023-05-10 11:53:32 +0700

    feat: Add env

--- connection.js
+++ connection.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql');
+const env = require('dotenv').config();
+const {DB_HOST, DB_USER, DB_PASSWORD, DB_NAME} = process.env;
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'pokedex'
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME
 });
 
 connection.connect((err) => {
