ef932714bc33d598d6d84b7abcc5d5ed8a6ed659
sintariaulia <sintariaulia04@gmail.com>
2023-05-10 16:32:23 +0700

    feat: connection.js

--- connection.js
+++ connection.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql');
 const env = require('dotenv').config();
+const {DB_HOST, DB_USER, DB_PASSWORD, DB_NAME} = process.env;
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  port: DB_PORT
 });
 
 connection.connect((err) => {
