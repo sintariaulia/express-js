d173d6c16ead1eca39e6acf264a669d9f06983bf
sintariaulia <sintariaulia04@gmail.com>
2023-05-11 16:01:22 +0700

    fix: edit file pokemons route

--- routes/pokemons.js
+++ routes/pokemons.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var connection = require('../connection');
 
-router.get('/pokemons', (req, res) => {
+router.get('/pokemon', (req, res) => {
   const sql = 'SELECT * FROM pokemons';
   connection.query(sql, (err, results) => {
     if (err) throw err;
@@ -10,7 +10,7 @@ router.get('/pokemons', (req, res) => {
   });
 });
 
-router.get('/pokemons/:id', (req, res) => {
+router.get('/pokemon/:id', (req, res) => {
   const pokemonId = req.params.id
   const sql = 'SELECT * FROM pokemons WHERE id = ?';
   connection.query(sql, [pokemonId], (err, results) => {
@@ -23,7 +23,7 @@ router.get('/pokemons/:id', (req, res) => {
   });
 });
 
-router.post('/pokemons', (req, res) => {
+router.post('/pokemon', (req, res) => {
   const { id, name, avatar, moves } = req.body;
   const sql = 'INSERT INTO pokemons (id, name, avatar, moves) VALUES (?, ?, ?, ?)';
   connection.query(sql, [id, name, avatar, moves], (err, results) => {
@@ -34,7 +34,7 @@ router.post('/pokemons', (req, res) => {
   });
 });
 
-router.put('/pokemons/:id', (req, res) => {
+router.put('/pokemon/:id', (req, res) => {
   const pokemonId = req.params.id
   const { name, avatar, moves } = req.body
   const sql = 'UPDATE pokemons SET name = ?, avatar = ?, moves = ? WHERE id = ?';
@@ -47,7 +47,7 @@ router.put('/pokemons/:id', (req, res) => {
   });
 });
 
-router.delete('/pokemons/:id', (req, res) => {
+router.delete('/pokemon/:id', (req, res) => {
   const pokemonId = req.params.id
   const sql = 'DELETE FROM pokemons WHERE id = ?';
   connection.query(sql, [pokemonId], (err, results) => {
