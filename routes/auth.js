814661db26e8e90c8b2db4926ec784f0ab1eccf4
sintariaulia <sintariaulia04@gmail.com>
2023-05-15 19:37:57 +0700

    fix: add routing for auth register and login

---
+++ routes/auth.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var router = express.Router();
+var bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
+var User = require('../models/users');
+var authMiddleware = require('../middleware/auth');
+
+
+// Register
+router.post('/register', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Cek apakah email sudah terpakai
+    const existingUser = await User.findByEmail(email);
+    if (existingUser) {
+      return res.status(400).json({ message: 'Email already exists' });
+    }
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Buat user baru
+    const user = { email, password: hashedPassword };
+    const newUser = await User.create(user);
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+// Login
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Cari pengguna berdasarkan email
+    const user = await User.findByEmail(email);
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    // Bandingkan password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    // Generate token
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+// Protected route example
+router.get('/protected', authMiddleware, (req, res) => {
+  res.json({ message: 'Protected route accessed successfully' });
+});
+
+
+
+module.exports = router;
