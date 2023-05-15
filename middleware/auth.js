c6497331e8a31e176e7d4d16cadae9b0de997a1f
sintariaulia <sintariaulia04@gmail.com>
2023-05-15 17:10:59 +0700

    feat: create controller for register and login in file auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+var jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+    const token = req.headers.authorization;
+    if (!token) {
+        res.status(401).json({ message: 'Unauthorized' });
+        return;
+    }
+
+    try {
+        const decoded = jwt.verify(token, 'secret_key');
+        req.user = decoded;
+        next();
+    } catch (err) {
+        res.status(401).json({ message: 'Unauthorized' });
+    }
+};
+
+module.exports = authMiddleware;
+
